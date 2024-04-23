1db5450e0f34a04516d803fe7589d9697b92b959
Ahmed Hassan <ahmedff35@gmail.com>
2024-04-23 15:49:17 +0200

    ..

---
+++ js/home.js
@@ -0,0 +1,29 @@
+let aElements = document.querySelectorAll("a:not(.map)");
+
+aElements.forEach((aElement) => {
+
+    let overlay = document.createElement("div");
+    let span = aElement.querySelector('span');
+
+    overlay.style.position = "absolute";
+    overlay.style.top = "0";
+    overlay.style.left = "0";
+    overlay.style.width = "100%";
+    overlay.style.height = "100%";
+    overlay.style.background = "linear-gradient(to top, black, transparent)";
+    overlay.style.opacity = "0";
+    overlay.style.zIndex = "4";
+    overlay.style.transition = "opacity 0.5s";
+
+    aElement.appendChild(overlay);
+
+    aElement.addEventListener("mouseover", () => {
+        overlay.style.opacity = "1";
+        span.style.opacity = '1';
+    });
+
+    aElement.addEventListener("mouseout", () => {
+        overlay.style.opacity = "0";
+        span.style.opacity = '0';
+    });
+});
