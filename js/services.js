91a3616077a53d83991dfc1f1b8ca22dd6fa6ad1
Ahmed Hassan <ahmedff35@email.com>
2024-05-04 04:56:37 +0300

    ..

---
+++ js/services.js
@@ -0,0 +1,36 @@
+const popup = document.getElementById('popup-one');
+    const overlay = document.querySelector('.overlay-one');
+
+    function openPopup() {
+        popup.style.display = 'block';
+        overlay.style.display = 'block';
+    }
+
+    function closePopup() {
+        popup.style.display = 'none';
+        overlay.style.display = 'none';
+    }
+    const popup_two = document.getElementById('popup-two');
+    const overlay_two = document.querySelector('.overlay-two');
+
+    function openPopuptwo() {
+        popup_two.style.display = 'block';
+        overlay_two.style.display = 'block';
+    }
+
+    function closePopuptwo() {
+        popup_two.style.display = 'none';
+        overlay_two.style.display = 'none';
+    }
+    const popup_three = document.getElementById('popup-three');
+    const overlay_three = document.querySelector('.overlay-three');
+
+    function openPopupthree() {
+        popup_three.style.display = 'block';
+        overlay_three.style.display = 'block';
+    }
+
+    function closePopupthree() {
+        popup_three.style.display = 'none';
+        overlay_three.style.display = 'none';
+    }
