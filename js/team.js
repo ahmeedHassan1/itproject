c2b0bdc653c3e8b9b43d123c25411daa1a1ac058
Ahmed Hassan <ahmedff35@email.com>
2024-05-06 14:59:27 +0300

    ..

--- js/team.js
+++ js/team.js
@@ -1,12 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const teamMembers = document.querySelectorAll(".team-member");
+const teamMembers = document.querySelectorAll(".team-member");
 
-    teamMembers.forEach(function (member) {
-        member.addEventListener("click", function () {
-            const memberId = this.id;
-            const memberInfoDiv = document.getElementById("memberInfo");
-            memberInfoDiv.classList.remove("hidden");
-            memberInfoDiv.innerHTML = "<h2>Member Info</h2><p>Info about member " + memberId + "</p>";
-        });
+const memberInfo = {
+    'semsem': '20235016 shaghoof sahb sahbo w gd3',
+    'hassan': 'Info about hassan',
+    'fady': 'Info about fady',
+    'bahaa': 'Info about bahaa',
+    'asser': 'Info about asser',
+    'ayten': 'Info about ayten',
+    'basmala': 'Info about basmala',
+};
+
+teamMembers.forEach(function (member) {
+    member.addEventListener("click", function () {
+        const memberId = this.id;
+        const memberInfoDiv = document.getElementById("memberInfo");
+        memberInfoDiv.classList.remove("hidden");
+
+        const info = memberInfo[memberId];
+
+        memberInfoDiv.innerHTML = "<h2>Member Info</h2><p>" + info + "</p>";
     });
 });
